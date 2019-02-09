066f4b653e7e90457931665ba49d9084742410fa
Rohit <r4rohit002@gmail.com>
2019-02-09 18:18:23 +0530

    Added Splash Screen and updated Support libs and compile version

---
+++ Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Activities/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.freewifi.rohksin.freewifi.Activities;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.freewifi.rohksin.freewifi.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private LottieAnimationView into;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity_layout);
+
+        into = (LottieAnimationView)findViewById(R.id.bulucu_into);
+
+        into.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(SplashActivity.this, HomePageActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+
+
+    }
+
+}
