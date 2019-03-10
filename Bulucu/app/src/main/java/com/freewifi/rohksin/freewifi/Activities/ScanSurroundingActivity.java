a239f5f562e44fb82b8dae441df2758448c33e17
Rohit <r4rohit002@gmail.com>
2019-03-11 02:00:24 +0530

    Added ScanNum in Collapsing Toolbar when Toolbar is collapsed

--- Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Activities/ScanSurroundingActivity.java
+++ Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Activities/ScanSurroundingActivity.java
@@ -1,10 +1,12 @@
 package com.freewifi.rohksin.freewifi.Activities;
 
+import android.graphics.Color;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,12 +16,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
-import android.widget.TextClock;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.freewifi.rohksin.freewifi.Adapters.StringAdapter;
@@ -50,6 +49,7 @@ public class ScanSurroundingActivity extends AppCompatActivity {
     private LottieAnimationView scanLottieButton;
     private TextView wifiNum;
     private RelativeLayout scannerLayout;
+    private CollapsingToolbarLayout title;
 
 
     private ScanTask scanTask;
@@ -59,6 +59,9 @@ public class ScanSurroundingActivity extends AppCompatActivity {
     //private MenuItem item;
 
     private boolean SCAN_RUNNING = false;
+    private int SCAN_NUM =0;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -68,6 +71,31 @@ public class ScanSurroundingActivity extends AppCompatActivity {
 
         scannerLayout = (RelativeLayout)findViewById(R.id.scannerLayout);
         scannerLayout.setPadding(0,getStatusBarHeight(),0,0);
+
+        title= (CollapsingToolbarLayout)findViewById(R.id.title);
+        title.setExpandedTitleColor(Color.TRANSPARENT);
+
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    title.setTitle("Result Found "+SCAN_NUM);
+                    isShow = true;
+                } else if(isShow) {
+                    title.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+
 
 
 
@@ -202,8 +230,9 @@ public class ScanSurroundingActivity extends AppCompatActivity {
 
             // TEMP
             scanTime.setText(param[0]);
+            SCAN_NUM = scanResults.size();
+            wifiNum.setText("Result Found "+SCAN_NUM);
 
-            wifiNum.setText("Result Found "+scanResults.size());
             //item.setTitle(scanResults.size()+"");
             adapter = new StringAdapter(ScanSurroundingActivity.this, scanResults);
             rv.setAdapter(adapter);
