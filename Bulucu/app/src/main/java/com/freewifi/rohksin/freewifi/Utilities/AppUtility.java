4c39b542a8eb85c78209e21274b20297e6143a6e
Rohit <r4rohit002@gmail.com>
2019-03-16 17:47:45 +0530

    Added method to get statusBar height in utility

--- Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Utilities/AppUtility.java
+++ Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Utilities/AppUtility.java
@@ -49,5 +49,12 @@ public class AppUtility {
         return context.getResources().getString(resId);
     }
 
-
+    public static int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 }
