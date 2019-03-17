f1e3dcb7f55d85484974d5f18f6f0b843fa52fba
Rohit <r4rohit002@gmail.com>
2019-03-17 15:30:31 +0530

    Created iterface for Wifi Scan

---
+++ Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Interfaces/WifiScanInterface.java
@@ -0,0 +1,12 @@
+package com.freewifi.rohksin.freewifi.Interfaces;
+
+public interface WifiScanInterface {
+
+    public void startScan();
+
+    public void stopScan();
+
+    public void updateScanUI();
+
+
+}
