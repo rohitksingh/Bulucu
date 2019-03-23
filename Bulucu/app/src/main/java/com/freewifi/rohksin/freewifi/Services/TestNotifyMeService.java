5a514f61a7ad1e21900d1a78f5ccf2a4d93bac93
Rohit <r4rohit002@gmail.com>
2019-03-23 14:44:33 +0530

    Added WifiReceiver, Callback

--- Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Services/TestNotifyMeService.java
+++ Bulucu/app/src/main/java/com/freewifi/rohksin/freewifi/Services/TestNotifyMeService.java
@@ -1,19 +1,27 @@
 package com.freewifi.rohksin.freewifi.Services;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.freewifi.rohksin.freewifi.Interfaces.NotifyMeCallback;
 import com.freewifi.rohksin.freewifi.Interfaces.WifiScanInterface;
+import com.freewifi.rohksin.freewifi.Utilities.WifiUtility;
 
 public class TestNotifyMeService extends Service implements WifiScanInterface{
 
     private static final String TAG = "TestNotifyMeService";
-    
     private IBinder serviceBinder = new NotifyMeBinder();
+    private NotifyMeCallback notifyMeCallback;
+    private WifiManager wifiManager;
+    private WifiScanReceiver wifiScanReceiver;
     
     @Override
     public void onCreate()
@@ -25,6 +33,7 @@ public class TestNotifyMeService extends Service implements WifiScanInterface{
     public int onStartCommand(Intent intent, int flag, int flagId)
     {
         Log.d(TAG, "onStartCommand: ");
+        startScan();
         return START_STICKY;
     }
     
@@ -45,11 +54,17 @@ public class TestNotifyMeService extends Service implements WifiScanInterface{
     @Override
     public void startScan() {
         Log.d(TAG, "startScan: ");
+        wifiScanReceiver = new WifiScanReceiver();
+        registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager = WifiUtility.getSingletonWifiManager(this);
+        wifiManager.startScan();
+
     }
 
     @Override
     public void stopScan() {
         Log.d(TAG, "stopScan: ");
+        unregisterReceiver(wifiScanReceiver);
     }
 
     @Override
@@ -62,6 +77,39 @@ public class TestNotifyMeService extends Service implements WifiScanInterface{
         public TestNotifyMeService getService()
         {
             return TestNotifyMeService.this;
+        }
+    }
+
+
+    public String getTestMessage()
+    {
+        return "TestMessage";
+    }
+
+    public void registerCallBack(NotifyMeCallback notifyMeCallback)
+    {
+        this.notifyMeCallback = notifyMeCallback;
+    }
+
+
+    private class WifiScanReceiver extends BroadcastReceiver {
+
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+            {
+
+                Log.d(TAG, "onReceive: ");
+                WifiUtility.updateWifiResult(wifiManager.getScanResults());
+                wifiManager.startScan();                                                  // Contineous scan
+
+                //notifyUser(newResultFound(WifiUtility.getOpenScanResult()));
+
+            }
+
         }
     }
     
